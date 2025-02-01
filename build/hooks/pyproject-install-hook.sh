efbcb0c027dcb4239d738ee891915585347680dd
Feynman Liang <feynman.liang@gmail.com>
2025-01-31 16:06:10 -0800

    Update pyproject-install-hook.sh

--- build/hooks/pyproject-install-hook.sh
+++ build/hooks/pyproject-install-hook.sh
@@ -8,7 +8,7 @@ pyprojectInstallPhase() {
   pushd dist >/dev/null
 
   for wheel in ./*.whl; do
-    @uv@/bin/uv pip --offline --no-cache install --no-deps --link-mode=copy --python=@pythonInterpreter@ --python-platform=x86_64-unknown-linux-gnu --system --prefix "$out" $uvPipInstallFlags "$wheel"
+    @uv@/bin/uv pip --offline --no-cache install --no-deps --link-mode=copy --python=@pythonInterpreter@ --python-platform=aarch64-unknown-linux-gnu --system --prefix "$out" $uvPipInstallFlags "$wheel"
     echo "Successfully installed $wheel"
   done
 
